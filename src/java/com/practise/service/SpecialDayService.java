707947dad5c06c39d4c8ca847c7a281292faa835
Neha Gupta <neha.Gupta@ticgroup.com.au>
2019-09-06 15:16:00 +0530

    Adding few mocked implementations

--- src/java/com/practise/service/SpecialDayService.java
+++ src/java/com/practise/service/SpecialDayService.java
@@ -1,9 +1,18 @@
 package com.practise.service;
 
-import java.time.LocalDate;
+import com.practise.model.SpecialDay;
 
 public class SpecialDayService {
+
     public Boolean isTodaySpecialDay(){
+
+        //This is an mocked implementation
         return true;
+    }
+
+    public SpecialDay getTodaysDetail(){
+
+        //This is an mocked implementation
+        return new SpecialDay();
     }
 }
