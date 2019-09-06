713b2f51bca36c916e7b1515b781fc716cf99cc9
Neha Gupta <neha.Gupta@ticgroup.com.au>
2019-09-06 15:10:08 +0530

    adding the getter and setters.

--- src/java/com/practise/model/SpecialDay.java
+++ src/java/com/practise/model/SpecialDay.java
@@ -6,4 +6,28 @@ public class SpecialDay {
     private String name;
     private LocalDate date;
     private float fare;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public float getFare() {
+        return fare;
+    }
+
+    public void setFare(float fare) {
+        this.fare = fare;
+    }
 }
