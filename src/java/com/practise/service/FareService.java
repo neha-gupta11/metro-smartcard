653fa94ac16fe222c1227b2f66b6023f37c55323
Neha Gupta <neha.Gupta@ticgroup.com.au>
2019-09-06 16:10:38 +0530

    Fetching the right strategy for fetching the fare.

---
+++ src/java/com/practise/service/FareService.java
@@ -0,0 +1,27 @@
+package com.practise.service;
+
+import com.practise.Strategy.FareStrategy;
+import com.practise.Strategy.SpecialDayFareStrategy;
+import com.practise.Strategy.WeeklyFareStrategy;
+
+public class FareService {
+
+    private SpecialDayService specialDayService;
+
+    public float getTotalFare(int stationCount){
+        FareStrategy fareStrategy;
+        if(specialDayService.isTodaySpecialDay()){
+            fareStrategy=new SpecialDayFareStrategy();
+        }else {
+            fareStrategy=new WeeklyFareStrategy();
+        }
+        return calcFare(fareStrategy,stationCount);
+    }
+
+    private float calcFare(FareStrategy fareStrategy, int stationCount) {
+
+        return stationCount * fareStrategy.getFare();
+    }
+
+
+}
